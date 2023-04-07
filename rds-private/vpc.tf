e75f3aa667f4f75c7ca4cbdf9be3f1ca26e8cf16
Byron Jenkins <codeherk@gmail.com>
2023-04-06 20:15:16 -0400

    Format file

--- rds-private/vpc.tf
+++ rds-private/vpc.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "private_subnet_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet_range_a
   map_public_ip_on_launch = false
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
   tags = {
     "Name" = "${var.environment}-private-subnet-a"
   }
@@ -30,7 +30,7 @@ resource "aws_subnet" "private_subnet_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet_range_b
   map_public_ip_on_launch = false
-  availability_zone = "us-east-1b"
+  availability_zone       = "us-east-1b"
   tags = {
     "Name" = "${var.environment}-private-subnet-b"
   }
@@ -41,7 +41,7 @@ resource "aws_subnet" "public_subnet_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_range_a
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
   tags = {
     "Name" = "${var.environment}-public-subnet-a"
   }
@@ -92,14 +92,14 @@ resource "aws_security_group" "default" {
   description = "Default security group to allow inbound/outbound from the VPC"
   vpc_id      = aws_vpc.main.id
   depends_on  = [aws_vpc.main]
-  
+
   # ingress {
   #   from_port = "0"
   #   to_port   = "0"
   #   protocol  = "-1"
   #   self      = true
   # }
- 
+
   # Allow SSH for EC2 instances`
   # ingress {
   #   description = "Allow SSH"
@@ -143,7 +143,7 @@ resource "aws_security_group" "default" {
 
 # Allow inbound SSH for EC2 instances
 resource "aws_security_group_rule" "allow_ssh_in" {
-  description = "Allow SSH"
+  description       = "Allow SSH"
   type              = "ingress"
   from_port         = 22
   to_port           = 22
@@ -154,34 +154,34 @@ resource "aws_security_group_rule" "allow_ssh_in" {
 
 # Allow inbound HTTP for EC2 instances
 resource "aws_security_group_rule" "allow_http_in" {
-  description = "Allow inbound HTTP traffic"
+  description       = "Allow inbound HTTP traffic"
   type              = "ingress"
-  from_port = "80"
-  to_port   = "80"
-  protocol  = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = "80"
+  to_port           = "80"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.default.id
 }
 
 # Allow inbound HTTPS for EC2 instances
 resource "aws_security_group_rule" "allow_https_in" {
-  description = "Allow inbound HTTPS traffic"
+  description       = "Allow inbound HTTPS traffic"
   type              = "ingress"
-  from_port = "443"
-  to_port   = "443"
-  protocol  = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = "443"
+  to_port           = "443"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.default.id
 }
 
 # Allow all outbound traffic
 resource "aws_security_group_rule" "allow_all_out" {
-  description = "Allow outbound traffic"
+  description       = "Allow outbound traffic"
   type              = "egress"
-  from_port = "0"
-  to_port   = "0"
-  protocol  = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = "0"
+  to_port           = "0"
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.default.id
 }
 
@@ -195,7 +195,7 @@ resource "aws_db_subnet_group" "private_db_subnet" {
   name        = "mysql-rds-private-subnet-group"
   description = "Private subnets for RDS instance"
   # Subnet IDs must be in two different AZ. Define them explicitly in each subnet with the availability_zone property
-  subnet_ids  = ["${aws_subnet.private_subnet_a.id}", "${aws_subnet.private_subnet_b.id}"]
+  subnet_ids = ["${aws_subnet.private_subnet_a.id}", "${aws_subnet.private_subnet_b.id}"]
 }
 
 # RDS Security Group
@@ -204,7 +204,7 @@ resource "aws_security_group" "rds_sg" {
   description = "Allow inbound/outbound MySQL traffic"
   vpc_id      = aws_vpc.main.id
   depends_on  = [aws_vpc.main]
-  
+
   # # Allow MySQL connections 
   # ingress {
   #   description = "Allow TCP connection from instances with this security group"
@@ -217,13 +217,13 @@ resource "aws_security_group" "rds_sg" {
 
 # Allow inbound MySQL connections
 resource "aws_security_group_rule" "allow_mysql_in" {
-  description = "Allow inbound MySQL connections"
-  type              = "ingress"
-  from_port = "3306"
-  to_port   = "3306"
-  protocol  = "tcp"
+  description              = "Allow inbound MySQL connections"
+  type                     = "ingress"
+  from_port                = "3306"
+  to_port                  = "3306"
+  protocol                 = "tcp"
   source_security_group_id = aws_security_group.default.id
-  security_group_id = aws_security_group.rds_sg.id
+  security_group_id        = aws_security_group.rds_sg.id
 }
 
 
@@ -236,18 +236,18 @@ resource "aws_db_instance" "mysql_8" {
   # See storage comparision https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_Storage.html#storage-comparison
   storage_type = "gp2"
   # Specific Relational Database Software https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Welcome.html#Welcome.Concepts.DBInstance
-  engine         = "mysql"
+  engine = "mysql"
   # InvalidParameterCombination: RDS does not support creating a DB instance with the following combination: DBInstanceClass=db.t4g.micro, Engine=mysql, EngineVersion=5.7.41,
   # https://aws.amazon.com/about-aws/whats-new/2021/09/amazon-rds-t4g-mysql-mariadb-postgresql/
   engine_version = "8.0.32"
   # See instance pricing https://aws.amazon.com/rds/mysql/pricing/?pg=pr&loc=2
   instance_class = "db.t4g.micro"
-  
+
   # mysql -u dbadmin -h <ENDPOINT> -P 3306 -D sample -p
   # name is deprecated, use db_name instead
-  db_name              = "sample"
-  username             = "dbadmin"
-  password             = "<PASSWORD-HERE>"
+  db_name  = "sample"
+  username = "dbadmin"
+  password = "<PASSWORD-HERE>"
   # parameter_group_name = "default.mysql8.0.32"
   # Name of DB subnet group. DB instance will be created in the VPC associated with the DB subnet group.
   db_subnet_group_name = aws_db_subnet_group.private_db_subnet.name
@@ -265,7 +265,7 @@ resource "aws_db_instance" "mysql_8" {
 ###########################
 resource "aws_instance" "go_api" {
   # https://cloud-images.ubuntu.com/locator/ec2/
-  ami                         = "ami-0afb477ff8d65bb67"
+  ami = "ami-0afb477ff8d65bb67"
   # creating EC2 Instance: InvalidParameterValue: The architecture 'i386,x86_64' of the specified instance type does not match the architecture 'arm64' of the specified AMI.
   # aws ec2 describe-instance-types --filters Name=processor-info.supported-architecture,Values=arm64 --query "InstanceTypes[*].InstanceType" --output text
   instance_type               = "t4g.micro"
@@ -280,12 +280,12 @@ resource "aws_instance" "go_api" {
   root_block_device {
     delete_on_termination = true
     # iops                  = 150 # only valid for volume_type io1
-    volume_size           = 50
-    volume_type           = "gp2"
+    volume_size = 50
+    volume_type = "gp2"
   }
   tags = {
     Name = "go-api-mysql"
-    OS = "ubuntu"
+    OS   = "ubuntu"
   }
 
   depends_on = [aws_security_group.default, aws_key_pair.ec2_key_pair]
@@ -319,8 +319,8 @@ resource "tls_private_key" "rsa" {
 # Generates a local file 
 # https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file 
 resource "local_sensitive_file" "tf_key" {
-  content  = tls_private_key.rsa.private_key_pem
-  file_permission = "600"
+  content              = tls_private_key.rsa.private_key_pem
+  file_permission      = "600"
   directory_permission = "700"
-  filename = "${aws_key_pair.ec2_key_pair.key_name}.pem"
+  filename             = "${aws_key_pair.ec2_key_pair.key_name}.pem"
 }
